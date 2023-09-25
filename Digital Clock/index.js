d87337ea9fb75b2b57327e6910746c6ff970960f
user.email <jobayer.ahmed109@gmail.com>
2023-09-25 22:40:45 +0600

    Display Digital clock project has successfully done

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -1,1 +1,30 @@
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let amPmEl = document.getElementById("amPm");
 
+function digitalClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+    amPm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  amPmEl.innerText = amPm;
+
+  setTimeout(() => {
+    digitalClock();
+  }, 1000);
+}
+
+digitalClock();
