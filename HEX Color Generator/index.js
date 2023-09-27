42b6a2b07f211c0ca192694d5c1fe4bbeb635642
user.email <jobayer.ahmed109@gmail.com>
2023-09-27 07:11:44 +0600

    Toast message function create and showed msg in doc body

--- HEX Color Generator/index.js
+++ HEX Color Generator/index.js
@@ -4,9 +4,13 @@
       1. Generate Hex color and change the the background color.
       2. Display the HEX code in the UI.
       3. Copy the the HEX color code by clicking a 'copy button'.
+      4. Show a toast message when copied.
 
 */
 
+// ---------------------------------------------------------------->>
+
+// Onload method applied here to help the auto browser reload.
 window.onload = () => {
   main();
 };
@@ -17,21 +21,33 @@ function main() {
   const root = document.getElementById("root");
   const output = document.getElementById("input");
 
+  // Change the BG color with random order.
   changeBtn.addEventListener("click", function (e) {
     const bgColor = generateHEXColor();
     root.style.backgroundColor = bgColor;
     output.value = bgColor;
   });
 
+  //  HEX color code copied by the help of browser navigator.
   copyBtn.addEventListener("click", function (e) {
     window.navigator.clipboard.writeText(output.value);
+
+    generateToastMsg(`${output.value} copied`);
   });
 }
 
+// Random HEX color code generator function.
 function generateHEXColor() {
   let red = Math.floor(Math.random() * 255);
   let green = Math.floor(Math.random() * 255);
   let black = Math.floor(Math.random() * 255);
 
   return `#${red.toString(16)}${green.toString(16)}${black.toString(16)}`;
+}
+
+// Toast message function
+function generateToastMsg(msg) {
+  let msgDiv = document.createElement("div");
+  msgDiv.innerText = msg;
+  document.body.appendChild(msgDiv);
 }
